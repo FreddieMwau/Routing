06a3ce39d9d6f30af4c3808f2f7b10a170697886
FreddieMwau <fredrickmutua12@gmail.com>
2022-05-23 04:54:52 +0300

    General implementation of adding components and services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddProductComponent } from './add-product/add-product.component';
 
-const routes: Routes = [];
+import { HomePageComponent } from './home-page/home-page.component';
+import { ProductsComponent } from './products/products.component';
+
+
+const routes: Routes = [
+  {path:"", component:HomePageComponent},
+  {path:"addProducts", component:AddProductComponent},
+  {path:"products", component:ProductsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ProductsComponent } from './products/products.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ProductComponent } from './products/product/product.component';
+import { EditProductComponent } from './products/edit-product/edit-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddProductComponent,
+    HomePageComponent,
+    ProductsComponent,
+    NotFoundComponent,
+    ProductComponent,
+    EditProductComponent
   ],
   imports: [
     BrowserModule,
